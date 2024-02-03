2a05587b81151a6ba81735058207252b6b11980b
Sean Eric So <scso@ucsc.edu>
2024-02-03 15:39:29 -0800

    Implementing AmmoManager

---
+++ Assets/Managers/AmmoManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoManager : MonoBehaviour
+{
+    public static AmmoManager instance;
+    private List<GameObject> fireBallList = new List<GameObject>();
+    [SerializeField] private GameObject fireObj; //[SerializeField] lets the variable be accessible to Unity editor, but not public to the rest
+    [SerializeField] private Transform fireBallParent;
+    [SerializeField] private float fireSpeed = 1000;
+    [SerializeField] private Transform fireBallSpawnpoint;
+
+    void Awake()
+    {
+        if(instance == null){
+            instance = this;
+        } else{
+            Destroy(this);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < 20; i++){
+            Vector3 spawnPoint = new Vector3(0, 0, 0);
+            GameObject fireBall = Instantiate(fireObj, spawnPoint, Quaternion.Euler(90, 0, 0), fireBallParent);
+            fireBall.SetActive(false);
+            fireBallList.Add(fireBall);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Fire(){ //Needs to be public so other Managers can access them
+        GameObject fireBallToSpawn = getFireBall();
+        if(fireBallToSpawn != null){
+            fireBallToSpawn.transform.position = fireBallSpawnpoint.position;
+            fireBallToSpawn.transform.rotation = transform.rotation;
+            fireBallToSpawn.SetActive(true);
+        }
+        //GameObject newFireBall = Instantiate(fireObj, fireballSpawnpoint.position, transform.rotation);
+        fireBallToSpawn.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * fireSpeed);
+    }
+
+    private GameObject getFireBall(){
+        for(int i = 0; i < 20; i++){ //Finding first object in the FireBall List that isn't active and returning it
+            if(!fireBallList[i].activeInHierarchy){
+                return fireBallList[i];
+            }
+        }
+        return null;
+    }
+}
---
+++ Assets/Managers/LootManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootManager : MonoBehaviour
+{
+    //LootManager Variable
+    public static LootManager instance;
+    private List<GameObject> coinList = new List<GameObject>();
+    private int maxCoins = 20;
+    [SerializeField] private GameObject coinPrefab; //[SerializeField] lets the variable be accessible to Unity editor, but not public to the rest
+    [SerializeField] private Transform coinParent;
+
+    void Awake()
+    {
+        if(instance == null){
+            instance = this;
+        } else{
+            Destroy(this);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < maxCoins; i++){
+            Vector3 randomSpawnPoint = new Vector3(Random.Range(-40, 41), 2.5f, Random.Range(-40, 41));
+            GameObject coin = Instantiate(coinPrefab, randomSpawnPoint, Quaternion.Euler(90, 0, 0), coinParent);
+            coin.SetActive(false);
+            coinList.Add(coin);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator spawnCoin(){
+        //StartCoroutine(spawnCoin());
+        yield return new WaitForSeconds(2f);
+        Vector3 randomSpawnPoint = new Vector3(Random.Range(-40, 41), 2.5f, Random.Range(-40, 41));
+        Instantiate(coinPrefab, randomSpawnPoint, Quaternion.Euler(90, 0, 0), coinParent); //Spawn coinPrefabs with 90 degree rotation; Assign parent to coinParent
+    }
+
+    public GameObject getCoin(){ //Based on Introduction To Object Pooling In Unity - https://www.youtube.com/watch?v=YCHJwnmUGDk
+        for(int i = 0; i < coinList.Count; i++){
+            if(!coinList[i].activeInHierarchy){
+                return coinList[i];
+            }
+        }
+
+        return null;
+    }
+}
