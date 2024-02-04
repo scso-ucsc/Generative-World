1d58026f3ddb0f52b59aa54383b3975ff5beaecf
Sean Eric So <scso@ucsc.edu>
2024-02-03 19:50:16 -0800

    Setting Coin Values

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -16,16 +16,4 @@ public class Coin : MonoBehaviour
     {
         
     }
-
-    // private void OnCollisionEnter(Collision collider){
-    //     } else if(collider.gameObject.tag == "Coin"){
-    //         collider.gameObject.SetActive(false);
-    //         this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero; //Resetting velocity to 0 so that if active again, that force no longer applies
-    //         this.gameObject.SetActive(false);
-    //     }
-    // }
-
-    public static void resetCoinValue(){
-        Debug.Log("Hello!");
-    }
 }
